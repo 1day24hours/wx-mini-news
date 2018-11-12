c8fdfe18c9b6805e81ae3c02a0965e15b6f38fbd
Felix Chen <chen.felix.xi@gmail.com>
2018-11-12 16:52:06 +0800

    2nd commit

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,2 +1,6 @@
 //index.js
 //获取应用实例
+const app = getApp()
+const network = require("../../utils/network.js")
+const NEWS_TYPE = ["gn", "gj", "cj", "yl", "js", "ty", "other"]
+const TAB_NAME = ["国内", "国际", "财经", "娱乐", "技术", "体育", "其它"]
